a7a4184839a8636693f9636cb58a9149a3281a38
Grant Wuerker <gwuerker@gmail.com>
2020-04-06 19:58:59 +0800

    Moved morphisms

--- src/AlgebraicStructures/Mappings.hs
+++ src/AlgebraicStructures/Mappings.hs
@@ -1,5 +1,6 @@
 module AlgebraicStructures.Mappings (
-  Mapping(..)
+  Mapping(..),
+  Morphism(..)
 ) where
 
 import AlgebraicStructures.Base
@@ -16,3 +17,11 @@ instance (Eq a, Eq b) => Ver (Mapping a b) where
   ver (Surjective f a b) = all (`elem` c) b && all (`elem` b) c
     where c = map f a
   ver (Bijective f a b)  = ver (Injective f a b) && ver (Surjective f a b)
+
+data Morphism a b =
+    Homomorphism (a -> a -> a) (b -> b -> b) [a] [b] (a -> b) |
+    Isomorphism  (a -> a -> a) (b -> b -> b) [a] [b] (a -> b)
+
+instance (Eq a, Eq b) => Ver (Morphism a b) where
+    ver (Homomorphism (+) (*) a b f) = and [f (x + y) == (f x) * (f y) && f (x + y) `elem` b | x <- a, y <- a]
+    ver (Isomorphism  (+) (*) a b f) = ver (Homomorphism (+) (*) a b f) && ver (Bijective f a b)
